46db577353af99f3fd6b99f59ccaceb0e4c9c918
Priya <priyannayak100@gmail.com>
2021-07-21 15:30:32 -0700

    added thanks and enter amount part

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+let body=document.querySelector("body");
 let menu=document.querySelector("#menu");
 let backBtn=document.querySelector("#back-btn");
+let main=document.querySelector("#main");
+let backthis=document.querySelector("#modal-selection");
+let radiobtn=document.querySelectorAll("input[name='back']");
+let continuebtn=document.querySelectorAll(".continue");
+let thanks=document.querySelector(".back1");
+let gotit=document.querySelector("#gotit");
 
- let main=document.querySelector("#main");
-// let body=document.querySelector("body");
-// let container=document.querySelector("#container");
 
 menu.addEventListener("click",showMenu);
 backBtn.addEventListener("click",backtoProject);
+radiobtn.forEach(a=>a.addEventListener('change',radiochange));
+continuebtn.forEach(a=>a.addEventListener('click',done));
+gotit.addEventListener("click",gotitfun);
 
 function showMenu(){
     document.querySelector(".menu-bar").classList.toggle("blocked");
@@ -20,9 +27,54 @@ function showMenu(){
 }
 
 function backtoProject(){
-    let backthis=document.querySelector(".backthis");
     backthis.classList.toggle("blocked");
+    main.classList.toggle("main-noclick"); 
+    let closeBtn=document.querySelector("#close");
+    closeBtn.addEventListener("click",backtoProject);
+}
 
-    main.classList.toggle("main-noclick");
-    backthis.style.opacity=1;
+
+function radiochange(event){
+    let parent=event.target.parentElement.parentElement.parentElement.parentElement;
+    let radioBlocks=document.querySelectorAll(".radioBlocks");
+    let elementhiddenBlock=document.getElementById(event.target.id+"-hidden");
+    let hiddenBlocks=document.querySelectorAll(".hidden-block-small");
+
+    console.log(hiddenBlocks)
+
+    radioBlocks.forEach(
+        a=>a.classList.contains("selected")
+        ?a.classList.remove("selected")
+        :a
+    )
+
+    radioBlocks.forEach(
+    a=>a==parent
+        ?a.classList.add("selected") 
+            ?a
+            :elementhiddenBlock!=null
+                ?elementhiddenBlock.classList.remove("blocked")
+                :a
+        :hiddenBlocks.forEach(
+            a=>a!=elementhiddenBlock
+            ?a.classList.add("blocked")
+            :a
+        )
+    )
+}
+
+
+function done(e)
+{
+    console.log(e.target);
+    backthis.classList.add("blocked");
+    thanks.classList.remove("blocked");
+    body.style.overflow="hidden";
+    document.documentElement.scrollTop = 30;
+}
+
+function gotitfun(){
+    body.style.overflow="scroll";
+    thanks.classList.add("blocked");
+    main.classList.toggle("main-noclick"); 
 }
