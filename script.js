d8d9193ce035e0b724a242683d5cf2ceeb9ca131
Priya <priyannayak100@gmail.com>
2021-07-22 10:12:02 -0700

    bookmark and select reward - completed mobile view

--- script.js
+++ script.js
@@ -7,13 +7,17 @@ let radiobtn=document.querySelectorAll("input[name='back']");
 let continuebtn=document.querySelectorAll(".continue");
 let thanks=document.querySelector(".back1");
 let gotit=document.querySelector("#gotit");
-
+let selectreward=document.querySelectorAll(".reward");
+let bookmark=document.querySelector("#bookmark");
 
 menu.addEventListener("click",showMenu);
 backBtn.addEventListener("click",backtoProject);
 radiobtn.forEach(a=>a.addEventListener('change',radiochange));
 continuebtn.forEach(a=>a.addEventListener('click',done));
 gotit.addEventListener("click",gotitfun);
+bookmark.addEventListener("click",bookmarked);
+
+selectreward.forEach(a=>a.addEventListener("click",display))
 
 function showMenu(){
     document.querySelector(".menu-bar").classList.toggle("blocked");
@@ -63,6 +67,13 @@ function radiochange(event){
     )
 }
 
+function display(e){
+    let id=e.target.id;
+    let el=document.getElementById(id+"-hidden");
+    let hide=document.querySelectorAll(".hide1");
+    hide.forEach(a=>a.classList.contains("blocked")?a:a.classList.add("blocked"));
+    el.classList.remove("blocked");
+}
 
 function done(e)
 {
@@ -71,10 +82,17 @@ function done(e)
     thanks.classList.remove("blocked");
     body.style.overflow="hidden";
     document.documentElement.scrollTop = 30;
+    main.classList.add("main-noclick"); 
 }
 
 function gotitfun(){
     body.style.overflow="scroll";
     thanks.classList.add("blocked");
     main.classList.toggle("main-noclick"); 
+}
+
+function bookmarked(e){
+    e.target.src.includes("icon-bookmark.svg")
+    ?e.target.src="images/icon-bookmark-selected.svg"
+    :e.target.src="images/icon-bookmark.svg"
 }
